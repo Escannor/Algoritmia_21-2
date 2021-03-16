3480ba37bad18d7bf64596a376b456b17a3338e6
Arturo <43208414+Escannor@users.noreply.github.com>
2021-03-16 14:58:50 -0600

    repaso S6

--- S6/repaso.py
+++ S6/repaso.py
@@ -2,25 +2,35 @@ import random
 
 def repaso(nombre, num_preguntas):
     aciertos = 0
+    operaciones = ["+", "-", "*", "/"]
+
     for i in range(num_preguntas):
         a = random.randint(1,9)
         b = random.randint(1,9)
-        print(f"{a}+{b} = ")
-        resultado = int(input())
-        if validar(a, b, '+', resultado):
+        oper = random.randint(0,3)
+
+
+        resultado = input(f"{a}{}{b} = ")
+
+        if validar(a, b, operaciones[oper], resultado):
             aciertos += 1
+
 
 
     print(f"{nombre} ha conseguido {aciertos}/{num_preguntas}")
 
 
-
-def validar(a,b,operador, c):  # 4, 5, '*', 20    | 1,3, '/', 0 | + | -
+# Funcion a terminar
+def validar(a, b, operador, c):  # 4, 5, '*', 20    | 1,3, '/', 0 | + | -
+    """Valida una operacion"""
     if operador == '+':
         if (a + b) == c:
             return True
         else:
             return False
+            
+
+    return False
 
 
 
