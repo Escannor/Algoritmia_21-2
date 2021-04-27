284e35a040eae7aa1a0a6cb9336f0d4c2b782a48
Arturo <43208414+Escannor@users.noreply.github.com>
2021-04-27 14:33:16 -0500

    Correcion S8

--- S8/EntregableS8.py
+++ S8/EntregableS8.py
@@ -4,21 +4,29 @@ class Examen:
         self.materia = materia
         self.calificacion = calificacion
 
+    def __str__(self):
+        return f'{self.materia} {self.calificacion}'
+
 class Alumno:
 
     def __init__(self, nombre):
         self.nombre = nombre
         self.examenes = []
     
-    def agregarExamen(self, examen):
-        self.examenes.append(examen)
+    def agregarExamen(self, nombreExamen, calificacion):
+        self.examenes.append(Examen(nombreExamen, calificacion))
 
 
 
-listaMaterias = ['Matematicas', 'Ingles', 'Quimica']
+listaMaterias = ['Matematicas', 'Ingles', 'Quimica', 'Historia']
 
 luis = Alumno('luis')
 i = 0
 while (i < len(listaMaterias)):
-    luis.agregarExamen(listaMaterias[i])  #Mandar Objeto examen
+    calificacion = int(input(f'Ingrese calificacion para {listaMaterias[i]}: '))
+    luis.agregarExamen(listaMaterias[i], calificacion)  #Mandar Objeto examen
     i+=1 
+
+print(f'\nExamenes de {luis.nombre}')
+for examen in luis.examenes:
+    print(examen)
